a2689807db79416a428195eb475a19c4db122580
GugaSantos <guga.santos.guga@gmail.com>
2022-09-08 17:54:33 -0300

    feat: add 'muito sodio' to products where sodio.percentual was greater to 39

---
+++ challenges/desafio25.js
@@ -0,0 +1,10 @@
+// use('commerce')
+
+db.produtos.updateMany(
+  { valoresNutricionais: 
+    { $elemMatch: 
+      { tipo: "sódio", percentual: { $gte: 40 } } } },
+  { $addToSet: { tags: "muito sódio" } })
+
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 })
