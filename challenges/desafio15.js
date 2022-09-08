a5d9dfb912a2bfe4ac214f76f73b46e13c1c6833
GugaSantos <guga.santos.guga@gmail.com>
2022-09-08 17:02:56 -0300

    feat: Add and update avaliation attribute

---
+++ challenges/desafio15.js
@@ -0,0 +1,13 @@
+// use('commerce')
+
+db.produtos.updateMany({}, { $set: { avaliacao: 0 }})
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino"] }},
+  { $inc: { avaliacao: +5 } })
+
+  db.produtos.updateMany(
+  { tags: { $in: ["ave"] }},
+  { $inc: { avaliacao: +3 } })
+
+db.produtos.find({}, {_id: 0, nome: 1, avaliacao: 1})
