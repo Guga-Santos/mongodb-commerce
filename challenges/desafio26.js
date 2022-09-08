4a4acc53c0b21c66eec4ec8c4a33d5a68fdff38b
GugaSantos <guga.santos.guga@gmail.com>
2022-09-08 17:57:09 -0300

    feat: add 'contem sodio' to products that have percentual greater than 20 and lower than 40

---
+++ challenges/desafio26.js
@@ -0,0 +1,11 @@
+// use('commerce')
+
+db.produtos.updateMany(
+  { valoresNutricionais: 
+    { $elemMatch: 
+      { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } },
+  { $addToSet: { tags: "contém sódio" } })
+
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 })
+
