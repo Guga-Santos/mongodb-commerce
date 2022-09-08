12afaacf2b69be2790d9b9c68a57a2bb7de94afd
GugaSantos <guga.santos.guga@gmail.com>
2022-09-08 17:50:24 -0300

    feat: order in desc mode the attribute valoresNutricionais.percentual

---
+++ challenges/desafio24.js
@@ -0,0 +1,10 @@
+// use('commerce')
+
+db.produtos.updateMany(
+  {},
+  { $push: 
+    { valoresNutricionais:
+      { $each: [], $sort: { percentual: -1 } } } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 })
