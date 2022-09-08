23a3bf07ac0aa19854fb158e082e74c7bc243677
GugaSantos <guga.santos.guga@gmail.com>
2022-09-08 17:20:15 -0300

    feat: add bacon to ingredients of Big Mac and Quarteirao com queijo

---
+++ challenges/desafio18.js
@@ -0,0 +1,8 @@
+// use('commerce')
+
+db.produtos.updateMany(
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+  { $addToSet: { ingredientes: "bacon" } },
+);
+
+db.produtos.find({},{ _id: 0, nome: 1, ingredientes: 1});
