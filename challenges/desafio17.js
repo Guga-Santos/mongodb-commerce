eb9a09226a4161f43ddb99153c4e1ce48faffe52
GugaSantos <guga.santos.guga@gmail.com>
2022-09-08 17:15:00 -0300

    feat: create resumoProdutos Collection and add a new document

---
+++ challenges/desafio17.js
@@ -0,0 +1,10 @@
+// use('commerce')
+
+db.createCollection('resumoProdutos')
+
+db.resumoProdutos.insertOne({
+  franquia: 'McDonalds',
+  totalProdutos: db.produtos.countDocuments({})
+})
+
+db.resumoProdutos.find({}, {_id: 0, franquia: 1, totalProdutos: 1})
