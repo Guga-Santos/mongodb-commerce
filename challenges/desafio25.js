84af50fab98300704f757f6669b9176c1efacb32
GugaSantos <guga.santos.guga@gmail.com>
2022-10-27 21:50:27 -0300

    Feat: adding project to github

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -4,7 +4,7 @@ db.produtos.updateMany(
   { valoresNutricionais: 
     { $elemMatch: 
       { tipo: "sódio", percentual: { $gte: 40 } } } },
-  { $addToSet: { tags: "muito sódio" } },
+      { $addToSet: { tags: "muito sódio" } },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
